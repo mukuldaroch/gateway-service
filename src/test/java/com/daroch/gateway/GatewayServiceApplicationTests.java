14b590dee62ef3851ec746932a478fdffc002576
mukuldaroch <2004bilaspuriya@gmail.com>
2026-01-19 18:03:32 +0530

    Initial commit

---
+++ src/test/java/com/daroch/gateway/GatewayServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.daroch.gateway;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GatewayServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
